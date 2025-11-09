5a40737bf856284240aa8ad5968a44fd4df5825a
Alisson Diogo <alisson.diogo.soares@gmail.com>
2025-11-09 14:33:44 -0300

    feat: cenário e sprites

--- Assets/Scripts/Mechanics/PlayerMove.cs
+++ Assets/Scripts/Mechanics/PlayerMove.cs
@@ -1,36 +1,65 @@
+using System.IO;
 using UnityEngine;
 
 public class PlayerMoves : MonoBehaviour
 {
     private Rigidbody2D rigidbody2D;
 
-    [SerializeField] private readonly float speed = 10f;
+    [SerializeField] private readonly float speed = 5f;
+    [SerializeField] private float jumpForce = 8f;
+
+    private float direcao;
+    private bool isJumping;
 
     private Vector2 inputVector = new(0, 0);
 
     private void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        isJumping = false;
     }
 
     private void Update()
     {
-        ReceiveInput();
-    }
-
-    private void FixedUpdate()
-    {
         Move();
     }
 
+    //private void FixedUpdate()
+    //{
+    //    Move();
+    //}
+
     private void ReceiveInput()
     {
-        inputVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        
+
         Debug.Log(inputVector);
     }
 
     private void Move()
     {
-        rigidbody2D.linearVelocity = inputVector.normalized * speed;
+        //inputVector = new Vector2(Input.GetAxisRaw("Horizontal") * speed, rigidbody2D.linearVelocity.y);
+
+        //rigidbody2D.linearVelocity = inputVector.normalized;
+
+
+        direcao = Input.GetAxisRaw("Horizontal");
+        rigidbody2D.linearVelocity = new Vector2(direcao * speed, rigidbody2D.linearVelocityY);
+        
+
+
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            rigidbody2D.linearVelocityY = jumpForce;
+            isJumping = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Surface"))
+        {
+            isJumping = false;
+        }
     }
 }
